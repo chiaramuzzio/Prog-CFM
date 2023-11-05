9be2d311e9c765a97d331e2ad55832a068b3d99e
chiaramuzzio <cmuzzio@udesa.edu.ar>
2023-11-05 14:29:59 -0300

    api peliculas home prueba

--- js/index.js
+++ js/index.js
@@ -1,3 +1,56 @@
+let urlFavs = "https://api.themoviedb.org/3/trending/all/week?api_key=378786c706182646715863ed0e6d66cc";
+
+fetch(urlFavs)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    let results = data.results;
+    let fotos = ``;
+    let div = document.querySelector("#divPrueba");
+    for (let i = 0; i < 5; i++) {
+      let movie_id = results[i].id;
+      let movie_title = results[i].original_title;
+      let fecha = results[i].release_date;
+
+      let urlImgs = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=378786c706182646715863ed0e6d66cc`;
+
+      fetch(urlImgs)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          let posterPath = data.poster_path;
+          if (posterPath) {
+            fotos += `
+            <div class ="portada"> 
+                <div class="pelicula">
+                    <a href="./detail-movie.html" class="addPic"><img class="fotos" src="https://image.tmdb.org/t/p/original${posterPath}" alt="${movie_title}"></a>
+                    <div class="titfav">
+                        <h4 class="addTitle">${movie_title}</h4>
+                        <i class="fa-regular fa-heart" style="color: #ffffff;"></i>
+                    </div>
+                    <p class="addDate">Fecha de estreno: ${fecha}</p>
+                </div>    
+            </div>
+            `;
+          }
+
+          div.innerHTML = fotos;
+        })
+        .catch(function(error) {
+          console.log("Error al obtener imagen de película: " + error);
+        });
+    }
+  })
+  .catch(function(error) {
+    console.log("Error al obtener datos de películas: " + error);
+  });
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 /*let cora = document.querySelectorAll(".fa-heart");
 let arrayFavs = []
 let arrayFavsJSON = JSON.stringify(arrayFavs)
