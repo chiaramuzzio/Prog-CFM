a5d398d51b1c08e2477f34c9f88d84e174d567b6
chiaramuzzio <cmuzzio@udesa.edu.ar>
2023-11-07 14:37:50 -0300

    intento detalle generos

--- js/genres.js
+++ js/genres.js
@@ -2,22 +2,34 @@ let api_key = "378786c706182646715863ed0e6d66cc"
 let generoPeliculas = `https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}`
 let generoSeries = `https://api.themoviedb.org/3/genre/tv/list?api_key=${api_key}`
 
+function attachButtonListeners() {
+  let botones = document.querySelectorAll(".button");
+  for (let i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("click", function() {
+      let idBoton = botones[i].id;
+      console.log("Valor del botón: " + idBoton);
+      localStorage.setItem("id", JSON.stringify(idBoton));
+    });
+  }
+}
+
 fetch(generoPeliculas)
 .then(function(response) {
   return response.json()
 })
 .then(function(data) {
     let genre = data.genres
+    console.log(data);
     let contenido = ''
     let divPeliculas = document.querySelector("#genero-peli")
     
     for(let i=0; i<genre.length; i++){
         let id = genre[i].id
         let name = genre[i].name
-        contenido += `<button class="button" type="submit" name="${name}" id="${id}">${name}</button>`
+        contenido += `<button class="button" type="submit" name="boton" value="${name}" id="${id}">${name}</button>`
     }
     divPeliculas.innerHTML = contenido
-  console.log(data);
+    attachButtonListeners();
 })
 .catch(function(error) {
   console.log("Error: " + error);
@@ -39,10 +51,9 @@ fetch(generoSeries)
         contenido += `<button class="button" type="submit" name="${name}" id="${id}">${name}</button>`
     }
     divPeliculas.innerHTML = contenido
-  console.log(data);
+  attachButtonListeners();
+
 })
 .catch(function(error) {
   console.log("Error: " + error);
 })
-
-
